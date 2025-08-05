27e9e4e704a3d1a6835ee08ea8b980266f0c2eda
200~Aryan-17806 <aryansingh17806@gmail.com>
2025-08-05 13:26:26 +0530

    fifth commit

--- first.java
+++ first.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class first{
     public static void main(String[] args) {
-        System.out.println("Hello world of git and github");
-    }
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int [] arr = new int[n];
+        for(int i = 0 ; i < n ; i ++) arr[i] = input.nextInt();
+        input.close();
+        MergeSort(arr);
+        print(arr);
+    }   
+        public static void print(int[] arr) {
+            for(int i = 0 ; i < arr.length ; i++) System.out.print(arr[i] + " ");
+        }
+        public static void MergeSort(int[] arr) {
+            int n = arr.length;
+            if(n == 1) return;
+            int [] a = new int[n/2];
+            int [] b = new int[n - n/2];
+            // a.length = n/2 & b.length = n - n/2
+            for(int i = 0 ; i < a.length ; i++) a[i] = arr[i];
+            for(int i = 0 ; i < b.length ; i++) b[i] = arr[i+n/2];
+            MergeSort(a);
+            MergeSort(b);
+            merge(a,b,arr);
+            a = null; b = null;
+        }
+        public static void merge(int [] left,int [] right,int [] result) {
+                int i = 0 , j = 0 , k = 0;
+                while(i < left.length && j < right.length){
+                    if(left[i] <= right[j]) result[k++] = left[i++];
+                    else result[k++] = right[j++];
+                }
+                while (i < left.length) result[k++] = left[i++];
+                while (j < right.length) result[k++] = right[j++];
+        }
 }
